af5e3354f99058b206b5308107935d1bdc9c8e99
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-22 20:14:33 -0600

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flavor){
+  return "I love " + flavor;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
